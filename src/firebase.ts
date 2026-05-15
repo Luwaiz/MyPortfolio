fe919c421bb0e61975fb6df6bdcc4eb6f786021c
Eluwa Emmanuel <emmaeluwa2021@gmail.com>
2026-05-15 11:49:14 +0100

    fixed env

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyZQ_MOfTRVKszrTlyuq9c2yvLxoszzfE",
-  authDomain: "cibo-a0869.firebaseapp.com",
-  projectId: "cibo-a0869",
-  storageBucket: "cibo-a0869.firebasestorage.app",
-  messagingSenderId: "29025198445",
-  appId: "1:29025198445:web:96ff794f6cc8bd5e5aba64",
-  measurementId: "G-377NJ9J9MV",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
